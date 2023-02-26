2aeb77e3eda9e91082c0319a88be5bb67fbba89a
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2023-02-26 21:56:04 +0000

    Update presets.js

--- sim/scripts/RD/presets.js
+++ sim/scripts/RD/presets.js
@@ -131,6 +131,26 @@ break;
 };
 break;
 		  
+	case "GiererMeinhardtStripes":
+	options = {
+	"autoSetColourRange": true,
+	"clearValueV": "1",
+	"diffusionStrUU": "0.00005",
+	"diffusionStrVV": "0.00005*D",
+	"diffusionStrWW": "0",
+	"dt": 0.0005,
+	"kineticParams": "a=0.5;b=1;c=6.1;K=0.003;D=100",
+	"maxColourValue": 9.410100936889648,
+	"minColourValue": 1.0677992105484009,
+	"preset": "PRESETNAME",
+	"reactionStrU": "a+u^2/(v*(1+K*u^2))-b*u",
+	"reactionStrV": "u^2-c*v",
+	"reactionStrW": "0",
+	"whatToDraw": "u",
+	"whatToPlot": "u",
+};
+break;
+		  
     case "localisedPatterns":
       options = {
         brushValue: "9",
