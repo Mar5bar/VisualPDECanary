d6bac147fed348907076f8163771dbdded08107d
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-03-16 09:51:43 +0000

    Brush can now use kinetic params and the values of u,v,w, as well as x,y

--- sim/scripts/RD/drawing_shaders.js
+++ sim/scripts/RD/drawing_shaders.js
@@ -3,23 +3,37 @@
 export function drawShaderTop() {
   return `varying vec2 textureCoords;
     uniform sampler2D textureSource;
-	uniform sampler2D imageSource;
+	  uniform sampler2D imageSource;
     uniform vec2 brushCoords;
     uniform float brushRadius;
-    uniform float brushValue;
     uniform float domainWidth;
     uniform float domainHeight;
     uniform float seed;
     uniform float time;
+    uniform float dx;
+    uniform float dy;
     const float pi = 3.141592653589793;
+
+    float safepow(float x, float y) {
+      if (x >= 0.0) {
+          return pow(x,y);
+      }
+      if (mod(round(y),2.0) == 0.0) {
+          return pow(-x,y);
+      }
+      return -pow(-x,y);
+    }
 
     void main()
     {   
-        gl_FragColor = texture2D(textureSource, textureCoords);
+        vec4 uvw = texture2D(textureSource, textureCoords);
+        gl_FragColor = uvw;
         vec3 Tvec = texture2D(imageSource, textureCoords).rgb;
         float T = (Tvec.x + Tvec.y + Tvec.z) / 3.0;
 
         ivec2 texSize = textureSize(textureSource,0);
+        float x = textureCoords.x * float(texSize.x) * dx;
+        float y = textureCoords.y * float(texSize.y) * dy;
         vec2 diff = textureCoords - brushCoords;\n`;
 }
 
