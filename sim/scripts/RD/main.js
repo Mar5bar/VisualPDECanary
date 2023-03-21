99c02d11c03b3081ee1472001cf3daf7e0541268
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-03-21 10:17:05 +0000

    Render when changing to/from surface plot

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -1076,6 +1076,7 @@ function initGUI(startOpen) {
       .onChange(function () {
         configureGUI();
         configureCamera();
+        render();
       });
   }
   if (inGUI("threeDHeightScale")) {
