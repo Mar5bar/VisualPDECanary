bf8165eeddd293a5826450e372febca45730d9bb
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-03-28 14:57:23 +0100

    Parentheses added in diffusion computation

--- sim/scripts/RD/simulation_shaders.js
+++ sim/scripts/RD/simulation_shaders.js
@@ -87,9 +87,9 @@ export function RDShaderRobinY() {
 
 export function RDShaderUpdateNormal() {
     return `
-    float LDuuU = 0.5*(Duu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DuuR*(uvwR.r - uvw.r) + DuuL*(uvwL.r - uvw.r)) / dx / dx +  0.5*(Duu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DuuT*(uvwT.r - uvw.r) + DuuB*(uvwB.r - uvw.r)) / dy / dy;
-    float LDvvV = 0.5*(Dvv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DvvR*(uvwR.g - uvw.g) + DvvL*(uvwL.g - uvw.g)) / dx / dx +  0.5*(Dvv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DvvT*(uvwT.g - uvw.g) + DvvB*(uvwB.g - uvw.g)) / dy / dy;
-    float LDwwW = 0.5*(Dww*(uvwR.b + uvwL.b - 2.0*uvw.b) + DwwR*(uvwR.b - uvw.b) + DwwL*(uvwL.b - uvw.b)) / dx / dx +  0.5*(Dww*(uvwT.b + uvwB.b - 2.0*uvw.b) + DwwT*(uvwT.b - uvw.b) + DwwB*(uvwB.b - uvw.b)) / dy / dy;
+    float LDuuU = 0.5*((Duu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DuuR*(uvwR.r - uvw.r) + DuuL*(uvwL.r - uvw.r)) / dx) / dx +  0.5*((Duu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DuuT*(uvwT.r - uvw.r) + DuuB*(uvwB.r - uvw.r)) / dy) / dy;
+    float LDvvV = 0.5*((Dvv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DvvR*(uvwR.g - uvw.g) + DvvL*(uvwL.g - uvw.g)) / dx) / dx +  0.5*((Dvv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DvvT*(uvwT.g - uvw.g) + DvvB*(uvwB.g - uvw.g)) / dy) / dy;
+    float LDwwW = 0.5*((Dww*(uvwR.b + uvwL.b - 2.0*uvw.b) + DwwR*(uvwR.b - uvw.b) + DwwL*(uvwL.b - uvw.b)) / dx) / dx +  0.5*((Dww*(uvwT.b + uvwB.b - 2.0*uvw.b) + DwwT*(uvwT.b - uvw.b) + DwwB*(uvwB.b - uvw.b)) / dy) / dy;
 
     float du = LDuuU + f;
     float dv = LDvvV + g;
@@ -100,15 +100,15 @@ export function RDShaderUpdateNormal() {
 
 export function RDShaderUpdateCross() {
     return `
-    float LDuuU = 0.5*(Duu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DuuR*(uvwR.r - uvw.r) + DuuL*(uvwL.r - uvw.r)) / dx / dx +  0.5*(Duu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DuuT*(uvwT.r - uvw.r) + DuuB*(uvwB.r - uvw.r)) / dy / dy;
-    float LDuvV = 0.5*(Duv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DuvR*(uvwR.g - uvw.g) + DuvL*(uvwL.g - uvw.g)) / dx / dx +  0.5*(Duv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DuvT*(uvwT.g - uvw.g) + DuvB*(uvwB.g - uvw.g)) / dy / dy;
-    float LDuwW = 0.5*(Duw*(uvwR.b + uvwL.b - 2.0*uvw.b) + DuwR*(uvwR.b - uvw.b) + DuwL*(uvwL.b - uvw.b)) / dx / dx +  0.5*(Duw*(uvwT.b + uvwB.b - 2.0*uvw.b) + DuwT*(uvwT.b - uvw.b) + DuwB*(uvwB.b - uvw.b)) / dy / dy;
-    float LDvuU = 0.5*(Dvu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DvuR*(uvwR.r - uvw.r) + DvuL*(uvwL.r - uvw.r)) / dx / dx +  0.5*(Dvu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DvuT*(uvwT.r - uvw.r) + DvuB*(uvwB.r - uvw.r)) / dy / dy;
-    float LDvvV = 0.5*(Dvv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DvvR*(uvwR.g - uvw.g) + DvvL*(uvwL.g - uvw.g)) / dx / dx +  0.5*(Dvv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DvvT*(uvwT.g - uvw.g) + DvvB*(uvwB.g - uvw.g)) / dy / dy;
-    float LDvwW = 0.5*(Dvw*(uvwR.b + uvwL.b - 2.0*uvw.b) + DvwR*(uvwR.b - uvw.b) + DvwL*(uvwL.b - uvw.b)) / dx / dx +  0.5*(Dvw*(uvwT.b + uvwB.b - 2.0*uvw.b) + DvwT*(uvwT.b - uvw.b) + DvwB*(uvwB.b - uvw.b)) / dy / dy;
-    float LDwuU = 0.5*(Dwu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DwuR*(uvwR.r - uvw.r) + DwuL*(uvwL.r - uvw.r)) / dx / dx +  0.5*(Dwu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DwuT*(uvwT.r - uvw.r) + DwuB*(uvwB.r - uvw.r)) / dy / dy;
-    float LDwvV = 0.5*(Dwv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DwvR*(uvwR.g - uvw.g) + DwvL*(uvwL.g - uvw.g)) / dx / dx +  0.5*(Dwv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DwvT*(uvwT.g - uvw.g) + DwvB*(uvwB.g - uvw.g)) / dy / dy;
-    float LDwwW = 0.5*(Dww*(uvwR.b + uvwL.b - 2.0*uvw.b) + DwwR*(uvwR.b - uvw.b) + DwwL*(uvwL.b - uvw.b)) / dx / dx +  0.5*(Dww*(uvwT.b + uvwB.b - 2.0*uvw.b) + DwwT*(uvwT.b - uvw.b) + DwwB*(uvwB.b - uvw.b)) / dy / dy;
+    float LDuuU = 0.5*((Duu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DuuR*(uvwR.r - uvw.r) + DuuL*(uvwL.r - uvw.r)) / dx) / dx +  0.5*((Duu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DuuT*(uvwT.r - uvw.r) + DuuB*(uvwB.r - uvw.r)) / dy) / dy;
+    float LDuvV = 0.5*((Duv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DuvR*(uvwR.g - uvw.g) + DuvL*(uvwL.g - uvw.g)) / dx) / dx +  0.5*((Duv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DuvT*(uvwT.g - uvw.g) + DuvB*(uvwB.g - uvw.g)) / dy) / dy;
+    float LDuwW = 0.5*((Duw*(uvwR.b + uvwL.b - 2.0*uvw.b) + DuwR*(uvwR.b - uvw.b) + DuwL*(uvwL.b - uvw.b)) / dx) / dx +  0.5*((Duw*(uvwT.b + uvwB.b - 2.0*uvw.b) + DuwT*(uvwT.b - uvw.b) + DuwB*(uvwB.b - uvw.b)) / dy) / dy;
+    float LDvuU = 0.5*((Dvu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DvuR*(uvwR.r - uvw.r) + DvuL*(uvwL.r - uvw.r)) / dx) / dx +  0.5*((Dvu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DvuT*(uvwT.r - uvw.r) + DvuB*(uvwB.r - uvw.r)) / dy) / dy;
+    float LDvvV = 0.5*((Dvv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DvvR*(uvwR.g - uvw.g) + DvvL*(uvwL.g - uvw.g)) / dx) / dx +  0.5*((Dvv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DvvT*(uvwT.g - uvw.g) + DvvB*(uvwB.g - uvw.g)) / dy) / dy;
+    float LDvwW = 0.5*((Dvw*(uvwR.b + uvwL.b - 2.0*uvw.b) + DvwR*(uvwR.b - uvw.b) + DvwL*(uvwL.b - uvw.b)) / dx) / dx +  0.5*((Dvw*(uvwT.b + uvwB.b - 2.0*uvw.b) + DvwT*(uvwT.b - uvw.b) + DvwB*(uvwB.b - uvw.b)) / dy) / dy;
+    float LDwuU = 0.5*((Dwu*(uvwR.r + uvwL.r - 2.0*uvw.r) + DwuR*(uvwR.r - uvw.r) + DwuL*(uvwL.r - uvw.r)) / dx) / dx +  0.5*((Dwu*(uvwT.r + uvwB.r - 2.0*uvw.r) + DwuT*(uvwT.r - uvw.r) + DwuB*(uvwB.r - uvw.r)) / dy) / dy;
+    float LDwvV = 0.5*((Dwv*(uvwR.g + uvwL.g - 2.0*uvw.g) + DwvR*(uvwR.g - uvw.g) + DwvL*(uvwL.g - uvw.g)) / dx) / dx +  0.5*((Dwv*(uvwT.g + uvwB.g - 2.0*uvw.g) + DwvT*(uvwT.g - uvw.g) + DwvB*(uvwB.g - uvw.g)) / dy) / dy;
+    float LDwwW = 0.5*((Dww*(uvwR.b + uvwL.b - 2.0*uvw.b) + DwwR*(uvwR.b - uvw.b) + DwwL*(uvwL.b - uvw.b)) / dx) / dx +  0.5*((Dww*(uvwT.b + uvwB.b - 2.0*uvw.b) + DwwT*(uvwT.b - uvw.b) + DwwB*(uvwB.b - uvw.b)) / dy) / dy;
 
     float du = LDuuU + LDuvV + LDuwW + f;
     float dv = LDvuU + LDvvV + LDvwW + g;
