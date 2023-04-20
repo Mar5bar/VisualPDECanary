1df65546ea3b84a488d4e2885a91ad96e2fe2875
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-04-20 19:58:12 +0100

    Equation resizing corrected

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -4921,7 +4921,7 @@ function resizeEquationDisplay() {
     (count < 20) &
     ($("#equation_display")[0].getBoundingClientRect().right >=
       window.innerWidth - 50) &
-    ($("#equation_display")[0].getBoundingClientRect().right >
+    ($("#equation_display")[0].getBoundingClientRect().width >
       parseFloat($("#equation_display_box").css("min-width")))
   ) {
     fz = (parseFloat(el.css("font-size")) * 0.9).toString() + "px";
