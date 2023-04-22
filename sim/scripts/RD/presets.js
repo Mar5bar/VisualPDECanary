3c8b764a4f20930310b6e90d06ca1490ac2819a3
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-04-22 12:15:59 +0100

    Covid preset added

--- sim/scripts/RD/presets.js
+++ sim/scripts/RD/presets.js
@@ -3,44 +3,72 @@
 export function getPreset(id) {
   let options;
   switch (id) {
-      
-      
-      case "BeamEquation":
-	options = {
-	"algebraicW": true,
-	"boundaryConditionsU": "dirichlet",
-	"boundaryConditionsV": "dirichlet",
-	"boundaryConditionsW": "dirichlet",
-	"brushValue": "-8",
-	"brushRadius": "0.5",
-	"cameraTheta": 0.5,
-	"cameraPhi": 0,
-	"clearValueU": "0",
-	"crossDiffusion": true,
-	"diffusionStrUU": "D_c*D",
-	"diffusionStrVV": "0",
-	"diffusionStrVW": "-D",
-	"diffusionStrWU": "D",
-	"diffusionStrWW": "0",
-	"dimension": "1",
-	"drawIn3D": true,
-	"dt": 0.0001,
-	"kineticParams": "D  =  10;Q  =  0.000;C  =  0;D_c  =  0.1;",
-	"maxColourValue": 2.5,
-	"minColourValue": -8,
-	"numSpecies": "3",
-	"plotType": "line",
-	"preset": "BeamEquation",
-	"reactionStrU": "v",
-	"reactionStrV": "-Q-C*v",
-	"reactionStrW": "0",
-	"squareCanvas": true,
-	"typeOfBrush": "vline",
-	"whatToDraw": "u",
-	"whatToPlot": "u",
-	"undefined": true,
-};
-break;
+    case "BeamEquation":
+      options = {
+        algebraicW: true,
+        boundaryConditionsU: "dirichlet",
+        boundaryConditionsV: "dirichlet",
+        boundaryConditionsW: "dirichlet",
+        brushValue: "-8",
+        brushRadius: "0.5",
+        cameraTheta: 0.5,
+        cameraPhi: 0,
+        clearValueU: "0",
+        crossDiffusion: true,
+        diffusionStrUU: "D_c*D",
+        diffusionStrVV: "0",
+        diffusionStrVW: "-D",
+        diffusionStrWU: "D",
+        diffusionStrWW: "0",
+        dimension: "1",
+        drawIn3D: true,
+        dt: 0.0001,
+        kineticParams: "D  =  10;Q  =  0.000;C  =  0;D_c  =  0.1;",
+        maxColourValue: 2.5,
+        minColourValue: -8,
+        numSpecies: "3",
+        plotType: "line",
+        preset: "BeamEquation",
+        reactionStrU: "v",
+        reactionStrV: "-Q-C*v",
+        reactionStrW: "0",
+        squareCanvas: true,
+        typeOfBrush: "vline",
+        whatToDraw: "u",
+        whatToPlot: "u",
+        undefined: true,
+      };
+      break;
+    case "CovidInARoom":
+      options = {
+        boundaryConditionsU: "neumann",
+        brushAction: "smoothadd",
+        brushRadius: "20",
+        clearValueU: "0",
+        colourbar: true,
+        diffusionStrUU: "1",
+        diffusionStrVV: "0",
+        diffusionStrWW: "0",
+        domainScale: 250,
+        dt: 0.001,
+        kineticParams:
+          "V  =  20 in [0,40];R  =  10 in [0,20];lambda  =  1;s  =  0;d  =  0;eta_sus  =  0.1 in [0,1];rho_sus  =  0.1 in [0,1];I  =  1;",
+        minColourValue: -5.731946473019889e-9,
+        neumannStrU: "-H((dx-x))*u_x + H((x+dx-L_x))*u_x",
+        numSpecies: "2",
+        numTimestepsPerFrame: 200,
+        preset: "CovidInARoom",
+        reactionStrU:
+          "R*exp(-0.01*((x-L_x/2)^2+(y-L_y/2)^2))-(lambda+s+d)*u-V*u_x",
+        reactionStrV: "(1-eta_sus)*rho_sus*u",
+        reactionStrW: "0",
+        spatialStep: 1,
+        suppressTryClickingPopup: true,
+        timeDisplay: true,
+        whatToDraw: "u",
+        whatToPlot: "1 - exp(-I*v)",
+      };
+      break;
     case "maze":
       options = {
         boundaryConditionsU: "dirichlet",
@@ -364,7 +392,8 @@ break;
         diffusionStrVV: "D_r",
         diffusionStrWW: "0",
         dt: 0.001,
-        kineticParams: "b_i=0 in [-5,5];D_r=0.1;D_i=1;a_r=1;a_i=1;b_r=-1;n=10;m=10;",
+        kineticParams:
+          "b_i=0 in [-5,5];D_r=0.1;D_i=1;a_r=1;a_i=1;b_r=-1;n=10;m=10;",
         maxColourValue: 1.6760534048080444,
         minColourValue: 0.00015078618889674544,
         reactionStrU: "a_r*u-a_i*v+(b_r*u-b_i*v)*(u^2+v^2)",
