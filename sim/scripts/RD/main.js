180a8ac6919c43932eca9e23ec4a39b519f09963
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-05-11 23:39:33 +0100

    Update main.js

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -5657,7 +5657,7 @@ function configureViews() {
   // If there's no default view in options.views, add one.
   if (options.views.length == 0) {
     let view = buildViewFromOptions();
-    view.name = "Default";
+    view.name = "Default View";
     options.views.push(view);
   } else {
     // Fill in any unset parts of the views.
