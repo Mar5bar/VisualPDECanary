0d7804138d47d574d487d865c4c7a08b421d583e
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-05-30 16:01:46 +0100

    Script to add scrollToTop button added

---
+++ assets/js/scroll_to_top_button.js
@@ -0,0 +1,10 @@
+// If the document is long, add in a fading scroll-to-top button.
+if ($(document).height() > 2 * $(window).height()) {
+  const scrollToTopButton = document.createElement("a");
+  scrollToTopButton.classList.add("top-link");
+  scrollToTopButton.innerHTML = '<i class="fa-solid fa-circle-chevron-up"></i>';
+  scrollToTopButton.style = "hidden";
+  scrollToTopButton.href = "#";
+  scrollToTopButton.onclick = "scroll(0,0);";
+  document.body.appendChild(scrollToTopButton);
+}
