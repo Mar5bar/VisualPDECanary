f64e7356a7d831850d0bc30819f6084a5a41c5aa
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-05-30 16:41:20 +0100

    scroll to top now working as expected

--- assets/js/scroll_to_top_button.js
+++ assets/js/scroll_to_top_button.js
@@ -1,10 +1,17 @@
 // If the document is long, add in a fading scroll-to-top button.
 if ($(document).height() > 2 * $(window).height()) {
   const scrollToTopButton = document.createElement("a");
-  scrollToTopButton.classList.add("top-link");
+  scrollToTopButton.id = "top-link";
   scrollToTopButton.innerHTML = '<i class="fa-solid fa-circle-arrow-up"></i>';
-  scrollToTopButton.style = "hidden";
   scrollToTopButton.href = "#";
   scrollToTopButton.onclick = "scroll(0,0);";
   document.body.appendChild(scrollToTopButton);
+
+  window.onscroll = function () {
+    if ($(document).scrollTop() < 20) {
+      $(scrollToTopButton).fadeOut();
+    } else if (!$(scrollToTopButton).is(":visible")) {
+      $(scrollToTopButton).css("display", "flex").hide().fadeIn();
+    }
+  };
 }
