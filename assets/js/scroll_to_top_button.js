d22d5464b864d723fbaf55dfcb56febcc0072efc
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-05-30 16:17:56 +0100

    scroll_to_top_button updated and styled

--- assets/js/scroll_to_top_button.js
+++ assets/js/scroll_to_top_button.js
@@ -2,7 +2,7 @@
 if ($(document).height() > 2 * $(window).height()) {
   const scrollToTopButton = document.createElement("a");
   scrollToTopButton.classList.add("top-link");
-  scrollToTopButton.innerHTML = '<i class="fa-solid fa-circle-chevron-up"></i>';
+  scrollToTopButton.innerHTML = '<i class="fa-solid fa-circle-arrow-up"></i>';
   scrollToTopButton.style = "hidden";
   scrollToTopButton.href = "#";
   scrollToTopButton.onclick = "scroll(0,0);";
