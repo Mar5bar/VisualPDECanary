d006c2d072ef61f6bf8bc100b6b44e6f53921092
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-10-31 19:53:19 +0000

    Partial dark mode in sim

---
+++ assets/js/dark_mode.js
@@ -0,0 +1,26 @@
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+function toggleDarkMode(state, save) {
+  document.documentElement.classList.toggle("dark-mode", state);
+  localStorage.setItem("dark-mode", state);
+  if (save) localStorage.setItem("dark-mode-save-time", new Date());
+}
+if (localStorage.hasOwnProperty("dark-mode-save-time")) {
+  const now = new Date();
+  const then = new Date(localStorage.getItem("dark-mode-save-time"));
+  const diff = now - then;
+  // If the last save was more than 24 hours ago, remove the preference.
+  if (diff > 86400000) {
+    localStorage.removeItem("dark-mode");
+    localStorage.removeItem("dark-mode-save-time");
+  }
+}
+if (localStorage.hasOwnProperty("dark-mode")) {
+  toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+} else {
+  toggleDarkMode(useDark.matches);
+}
+// Listen for the changes in the OS settings, and remove preference on change.
+useDark.addEventListener("change", (evt) => {
+  toggleDarkMode(evt.matches);
+  localStorage.removeItem("dark-mode");
+});
