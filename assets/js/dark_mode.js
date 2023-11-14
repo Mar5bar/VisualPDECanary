aa59d6ba7cc66fa109e86d5842172a009ac3ab04
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-11-14 19:29:28 +0000

    Logo added to site. Dark mode now dark blue instead of grey

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -3,7 +3,7 @@ function toggleDarkMode(state, save) {
   document.documentElement.classList.toggle("dark-mode", state);
   localStorage.setItem("dark-mode", state);
   if (save) localStorage.setItem("dark-mode-save-time", new Date());
-  const themeColor = state ? "#202124" : "#dfdfdf";
+  const themeColor = state ? "#171b29" : "#dfdfdf";
   document
     .querySelector('meta[name="theme-color"]')
     .setAttribute("content", themeColor);
