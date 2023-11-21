0093316438f4c9d82b2e4d7b36de6fe947f8785e
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-11-21 11:56:22 +0000

    Another attempt at iOS file downloads...

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -3565,10 +3565,13 @@ import { Stats } from "../stats.min.js";
       var link = document.createElement("a");
       link.download = "VisualPDEScreenshot.png";
       renderer.render(scene, camera);
-      link.href = renderer.domElement.toDataURL();
-      document.body.appendChild(link);
+      link.href = renderer.domElement
+        .toDataURL()
+        .replace(
+          /^data:image\/?[A-z]*;base64,/,
+          "data:application/octet-stream;base64,"
+        );
       link.click();
-      document.body.removeChild(link);
       setSizes();
       render();
     }
@@ -9210,17 +9213,18 @@ import { Stats } from "../stats.min.js";
       // Attach the object URL to an <a> element, setting the download file name
       const a = document.createElement("a");
       a.style = "display: none;";
-      a.href = recording_url;
       a.download = "VisualPDERecording." + ext;
-      console.log(ext);
-      document.body.appendChild(a);
-      // Trigger the file download
-      a.click();
-      setTimeout(() => {
-        // Clean up. Firefox demands it be on a timeout.
-        URL.revokeObjectURL(recording_url);
-        document.body.removeChild(a);
-      }, 100);
+      const reader = new FileReader();
+      reader.onload = function () {
+        const b64 = reader.result.replace(/^data:.+;base64,/, "");
+        a.href = "data:application/octet-stream;base64," + b64;
+        a.click();
+        setTimeout(() => {
+          // Clean up. Firefox demands it be on a timeout.
+          URL.revokeObjectURL(recording_url);
+        }, 100);
+      };
+      reader.readAsDataURL(blob);
     };
 
     mediaRecorder.start();
