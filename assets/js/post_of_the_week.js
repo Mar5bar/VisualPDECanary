0b512dbf1950523a95c1272e841d00bc765b320d
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2024-02-24 20:07:09 +0000

    CSS fix for small screens for post of the day

--- assets/js/post_of_the_week.js
+++ assets/js/post_of_the_week.js
@@ -30,22 +30,37 @@ const days = Math.ceil(
 let postOfTheWeek = pages[days % pages.length];
 // Assign the content to the post of the week section.
 let post = document.getElementById("post-of-the-week");
+let container = document.getElementById("post-of-the-week-container");
 post.href = postOfTheWeek.url;
 post.getElementsByClassName("topic_banner")[0].style.backgroundImage =
   "url('" + postOfTheWeek.img + "')";
 post.getElementsByClassName("title")[0].textContent = postOfTheWeek.title;
 post.getElementsByClassName("subtitle")[0].textContent = postOfTheWeek.extract;
 
+// Set the size of the post.
+function setPostSize() {
+  let styles = window.getComputedStyle(post);
+  let margin =
+    parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
+  // If the container has flex-direction of column, add on the height of the text.
+  if (window.getComputedStyle(container).flexDirection === "column") {
+    let text = container.getElementsByTagName("p")[0];
+    margin += text.getBoundingClientRect().height;
+  }
+  document
+    .querySelector(":root")
+    .style.setProperty(
+      "--post-of-the-week-height",
+      post.getBoundingClientRect().height + margin + "px",
+    );
+}
+setPostSize();
+// Add a resize listener to adjust post size.
+window.addEventListener("resize", setPostSize);
+// Remove the transition property from the post container after 2s.
+setTimeout(() => {
+  container.style.transition = "none";
+}, 2000);
+
 // Make the post visible.
-let styles = window.getComputedStyle(post);
-let margin =
-  parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
-document
-  .querySelector(":root")
-  .style.setProperty(
-    "--post-of-the-week-height",
-    post.getBoundingClientRect().height + margin + "px",
-  );
-document
-  .getElementById("post-of-the-week-container")
-  .classList.remove("hiddenPost");
+container.classList.remove("hiddenPost");
