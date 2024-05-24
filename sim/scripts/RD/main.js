f639f15c537b09c12fdf8b9f561b3a824b9081f8
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2024-05-24 13:56:35 +0100

    T[x,y] for species lookup

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -4495,11 +4495,11 @@ import { createWelcomeTour } from "./tours.js";
     str = replaceBump(str);
     str = replaceWhiteNoise(str);
 
-    // Replace species(x,y) with a texture lookup. Note that this regex doesn't handle
-    // x being an expression containing a comma, and y cannot contain a closing parenthesis.
+    // Replace species[x,y] with a texture lookup. Note that this regex doesn't handle
+    // x being an expression containing a comma.
     str = str.replaceAll(
       new RegExp(
-        "\\b(" + anySpeciesRegexStrs[0] + ")\\(([^,]*),([^)]*)\\)",
+        "\\b(" + anySpeciesRegexStrs[0] + ")\\[([^,]*),([^\\]]*)\\]",
         "g",
       ),
       function (m, d1, d2, d3) {
