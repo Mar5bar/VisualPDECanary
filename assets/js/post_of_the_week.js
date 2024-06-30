57aed6468a7978e5f6b79e0ca17d38b1fe27f119
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2024-06-30 17:44:54 +0900

    Homepage has posts of the day. Simulator button appears on small screens

--- assets/js/post_of_the_week.js
+++ assets/js/post_of_the_week.js
@@ -27,40 +27,19 @@ const days = Math.ceil(
 );
 
 // Generate the post of the day. Note that adding new posts to the site will likely change the post of the week.
-let postOfTheWeek = pages[days % pages.length];
 // Assign the content to the post of the week section.
-let post = document.getElementById("post-of-the-week");
-let container = document.getElementById("post-of-the-week-container");
-post.href = postOfTheWeek.url;
-post.getElementsByClassName("topic_banner")[0].style.backgroundImage =
+let posts = document.getElementsByClassName("post-of-the-week");
+let index = 0;
+for (let post of posts) {
+  let postOfTheWeek = pages[(days + index) % pages.length];
+  post.href = postOfTheWeek.url;
+  post.getElementsByClassName("topic_banner")[0].style.backgroundImage =
   "url('" + postOfTheWeek.img + "')";
-post.getElementsByClassName("title")[0].textContent = postOfTheWeek.title;
-post.getElementsByClassName("subtitle")[0].textContent = postOfTheWeek.extract;
-
-// Set the size of the post.
-function setPostSize() {
-  let styles = window.getComputedStyle(post);
-  let margin =
-    parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
-  // If the container has flex-direction of column, add on the height of the text.
-  if (window.getComputedStyle(container).flexDirection === "column") {
-    let text = container.getElementsByTagName("p")[0];
-    margin += text.getBoundingClientRect().height;
-  }
-  document
-    .querySelector(":root")
-    .style.setProperty(
-      "--post-of-the-week-height",
-      post.getBoundingClientRect().height + margin + "px",
-    );
+  post.getElementsByClassName("title")[0].textContent = postOfTheWeek.title;
+  post.getElementsByClassName("subtitle")[0].textContent = postOfTheWeek.extract;
+  index++;
 }
-setPostSize();
-// Add a resize listener to adjust post size.
-window.addEventListener("resize", setPostSize);
-// Remove the transition property from the post container after 2s.
-setTimeout(() => {
-  container.style.transition = "none";
-}, 2000);
 
 // Make the post visible.
+let container = document.getElementById("post-of-the-week-container");
 container.classList.remove("hiddenPost");
