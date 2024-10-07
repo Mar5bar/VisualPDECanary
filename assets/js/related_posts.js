439c574edc773c0831c7fc8d2e6b17c34cfe9098
Benjamin Walker <7798410+Mar5bar@users.noreply.github.com>
2024-10-07 15:45:59 +0100

    Related posts now safe if none are found

--- assets/js/related_posts.js
+++ assets/js/related_posts.js
@@ -8,7 +8,7 @@ let pages = documents
 // Build the list of related posts, prioritising any specified in the frontmatter.
 // A related post is defined as one sharing categories with the current post. We'll sort by number of shared categories.
 let currentPost = pages.find((doc) => doc.url === window.location.pathname);
-let specifiedRelatedPosts = currentPost.related_posts
+let specifiedRelatedPosts = currentPost.related_posts;
 let relatedPosts = pages
   .map((doc) => {
     let sharedCats = doc.categories.filter((cat) =>
@@ -24,21 +24,23 @@ let relatedPosts = pages
   .map((post) => post.doc);
 
 // If there are specified related posts, add them to the beginning of the list by sorting by inclusion in related_posts.
-console.log(window.specifiedRelatedPosts);
 relatedPosts = relatedPosts.sort((a, b) => {
-    if (window.specifiedRelatedPosts.includes(a.url)) {
-        return -1;
-    } else if (window.specifiedRelatedPosts.includes(b.url)) {
-        return 1;
-    } else {
-        return 0;
-    }
-    });
+  if (window.specifiedRelatedPosts.includes(a.url)) {
+    return -1;
+  } else if (window.specifiedRelatedPosts.includes(b.url)) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
 
 // Assign the content to the post of the week section.
 let posts = document.getElementsByClassName("related_post");
 let index = 0;
 for (let post of posts) {
+  if (index >= relatedPosts.length) {
+    break;
+  }
   let relatedPost = relatedPosts[index];
   post.href = relatedPost.url;
   post.getElementsByClassName("topic_banner")[0].style.backgroundImage =
@@ -49,7 +51,11 @@ for (let post of posts) {
   index++;
 }
 
-// Make the post visible.
-let container = document.getElementById("related_posts-container");
-container.classList.remove("hiddenPost");
-document.getElementById("related_posts_wrapper").classList.remove("hiddenPost");
+// Make the post visible if a post was added.
+if (index > 0) {
+  let container = document.getElementById("related_posts-container");
+  container.classList.remove("hiddenPost");
+  document
+    .getElementById("related_posts_wrapper")
+    .classList.remove("hiddenPost");
+}
