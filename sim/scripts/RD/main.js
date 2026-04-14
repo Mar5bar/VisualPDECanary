3fb885f9a534e454081f50415ab8ef9d14e565d1
Benjamin Walker <7798410+Mar5bar@users.noreply.github.com>
2026-04-14 19:25:49 +0100

    Prevent overflow of precision in parameter slider (maxes out at 10 dp)

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -7366,10 +7366,10 @@ async function VisualPDE(url) {
           step = match[4];
           match[4] += ", ";
         }
-        controller.slider.precision = Math.max(
+        controller.slider.precision = Math.min(Math.max(
           controller.slider.precision,
           parseFloat(step).countDecimals(),
-        );
+        ), 10);
         controller.slider.step = step.toString();
 
         // Assign the initial value, which should happen after step has been defined.
