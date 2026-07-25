f1dba029878b2a0d48f5841020d2912d4ecd5565
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2026-07-25 13:02:26 +0000

    style: format files

--- sim/scripts/RD/main.js
+++ sim/scripts/RD/main.js
@@ -9438,7 +9438,7 @@ async function VisualPDE(url) {
     await waitListener(
       document.getElementById("updates_message_ok"),
       "click",
-      true
+      true,
     );
     $("#updates-message").css("display", "none");
     localStorage.setItem("seenUpdate:" + id, true);
